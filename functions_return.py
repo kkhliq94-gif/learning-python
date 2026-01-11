dcc1e02c327641041b94800dd17d477afa13f2cb
kkhliq94-gif <kkhliq94@gmail.com>
2026-01-12 00:20:10 +0530

    Learn return keyword with practice examples

---
+++ functions_return.py
@@ -0,0 +1,14 @@
+def multiply(a, b):
+    return a * b
+
+def square(n):
+    return n * n
+
+def is_even(num):
+    return num % 2 == 0
+
+
+print(multiply(4, 5))
+print(square(6))
+print(is_even(10))
+print(is_even(7))
