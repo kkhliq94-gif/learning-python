07493a9d8ab273438cd64de156f56ce0b8ce27d8
kkhliq94-gif <kkhliq94@gmail.com>
2026-01-11 10:15:40 +0530

    Add user input and number conversion example

---
+++ input_numbers.py
@@ -0,0 +1,7 @@
+name = input("اپنا نام لکھیں: ")
+age = int(input("اپنی عمر لکھیں: "))
+
+print("نام:", name)
+print("عمر:", age)
+print("اگلے سال عمر ہوگی:", age + 1)
+print("عمر کی قسم:", type(age))
