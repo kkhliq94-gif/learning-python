de39c5b39a52ff372eac2f4943200d84a1370e90
kkhliq94-gif <kkhliq94@gmail.com>
2026-01-11 20:16:03 +0530

    Practice: understanding what a function really is

---
+++ functions_practice.py
@@ -0,0 +1,18 @@
+# Function 1: simple greet
+def greet():
+    print("Hello! Welcome to Python functions.")
+
+# Function 2: introduce yourself
+def introduce(name):
+    print("My name is", name)
+
+# Function 3: add two numbers
+def add_numbers(a, b):
+    result = a + b
+    print("Sum is:", result)
+
+
+# ---- Function Calls ----
+greet()
+introduce("Abdul Khalique")
+add_numbers(5, 7)
